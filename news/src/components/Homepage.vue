b896aba8df0fadb58477cb44af27064ad7869b26
FayeCarter <fayecarter333@gmail.com>
2020-07-16 17:57:19 +0100

    Render key story elements

--- news/src/components/Homepage.vue
+++ news/src/components/Homepage.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h2>Homepage</h2>
-    <div v-for="story in stories" :key="story"><h2>{{ story }}</h2></div>
+    <div v-for="story in stories" :key="story">
+      <h2>{{ story.data.title }}</h2>
+      <p>{{ story.data.url }}}</p>
+      <p>Comments: {{ story.data.descendants }}</p>
+      <p>Score: {{ story.data.score }}</p>
+    </div>
   </div>
 </template>
 
@@ -19,7 +24,14 @@ export default {
     axios
       .get('https://hacker-news.firebaseio.com/v0/topstories.json')
       .then(result => {
-        this.stories = result.data
+        let response = result.data.slice(0, 10)
+        response.forEach(id => {
+          axios
+            .get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json')
+            .then(result => {
+              this.stories.push(result)
+            })
+        })
       })
       .catch((err) => {
         this.err = err
