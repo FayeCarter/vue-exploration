e12f7b62d9498be5207e68567642404efff9ec78
FayeCarter <fayecarter333@gmail.com>
2020-07-16 17:15:21 +0100

    Add navbar

---
+++ news/src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="pure-menu pure-menu-horizontal">
+    <di class="container">
+      <a href="/" class="pure-menu-heading pure-munu-link">Hacker News</a>
+      <ul class="pure-menu-list">
+        <li class="pure-menu-item"><a href="/" class="pure-menu-link">Upload</a></li>
+      </ul>
+    </di>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.pure-menu {
+  background-color: #E17842;
+}
+.pure-menu a {
+  color: white;
+}  
+</style>
