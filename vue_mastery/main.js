2d35788e66819c3af0efe1c0336201c0e2f05f41
FayeCarter <fayecarter333@gmail.com>
2020-07-16 14:47:30 +0100

    Update image on hover

--- vue_mastery/main.js
+++ vue_mastery/main.js
@@ -2,21 +2,31 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    image: './images/socks.jpg',
+    image: './images/blue-socks.jpg',
     inventory: 10,
     onSale: true,
     details: ["80% cotton", "20% polyester"],
     variants: [
       {
         variantID: 2234,
-        variantColor: "green"
+        variantColor: "green",
+        variantImage: "./images/green-socks.jpg"
       },
       {
         variantID: 2235,
-        variantColor: "blue"
+        variantColor: "blue",
+        variantImage: "./images/blue-socks.jpg"
       }
     ],
     sizes: ["tiny", "small", "medium", "big", "massive"],
     cart: 0
+  },
+  methods: {
+    addToCart: function () {
+      this.cart += 1
+    },
+    updateProduct: function (variantImage) {
+      this.image = variantImage
+    }
   }
 })
