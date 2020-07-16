42bff262b937cdca5391ec9d4fe3c34385d43cdb
FayeCarter <fayecarter333@gmail.com>
2020-07-16 17:50:01 +0100

    return story ids

--- news/src/components/Homepage.vue
+++ news/src/components/Homepage.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
     <h2>Homepage</h2>
+    <div v-for="story in stories" :key="story"><h2>{{ story }}</h2></div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'Homepage'
+  name: 'Homepage',
+  data: function () {
+    return {
+      err: '',
+      stories: []
+    }
+  },
+  created: function () {
+    axios
+      .get('https://hacker-news.firebaseio.com/v0/topstories.json')
+      .then(result => {
+        this.stories = result.data
+      })
+      .catch((err) => {
+        this.err = err
+      })
+  }
 }
 </script>
 
